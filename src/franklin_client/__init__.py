9576b1ad81bb9a000d11aad11abb62b6e91650ee
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2024-02-08 14:30:35 +0100

    Add get_analysis_status

--- src/franklin_client/__init__.py
+++ src/franklin_client/__init__.py
@@ -20,8 +20,7 @@ class Franklin(object):
         self.auth = FranklinAuth(self.api_uri, email, password)
 
     def _get(self, endpoint, params=None, **kwargs):
-        """
-        Get data from the end_point, combining base_uri, api uri and end_point. Return the response as decoded json
+        """Get data from the end_point, combining api uri and end_point. Return the response as decoded json
 
         :param end_point: end point to get data from
         :param params: Optional params dict
@@ -32,16 +31,27 @@ class Franklin(object):
         response.raise_for_status()  # Raise exception on request error
         return response.json()
 
+    def _post(self, endpoint, data=None, **kwargs):
+        """Post data to the end_point, combining api uri and end_point. Return the response as decoded json
+
+        :param end_point: end point to post data to
+        :param data: Optional dictionary, list of tuples, bytes, or file-like object
+        """
+        uri = f'{self.api_uri}/{endpoint}'
+        response = requests.post(uri, json=data, auth=self.auth, **kwargs)
+        response.raise_for_status()
+        return response.json()
+
     def get_assay_list(self):
         """Get a list of all organization assays."""
         return self._get(endpoint='assay/list')['assays']  # Note: Should we return the whole response or just the assays list?
 
     def get_analysis_list(self, analysis_name=None, status=None, created_before=None, created_after=None, assay_id=None):
-        """Get a list of all analyses for each assay.
+        """Get a list of all analyses for each assay
 
         :param analysis_name: Optional filter by the name of the analysis,
             will return analysis where name contains the partial string
-        :param status: Optional filter by the status of the analysis (active\suspended\resolved\creating)
+        :param status: Optional filter by the status of the analysis (active, suspended, resolved or creating)
         :param created_before: Optional filter by the date of creation, before (yyyy-MM-dd)
         :param created_after: Optional filter by the date of creation, after (yyyy-MM-dd)
         :param assay_id: Optional filter by the assay id
@@ -54,3 +64,10 @@ class Franklin(object):
             'assay_id': assay_id
         }
         return self._get(endpoint='analyses/list', params=params)['analyses_by_assay']
+
+    def get_analysis_status(self, analysis_ids):
+        """Get the status of analysis by ids.
+
+        :param analysis_ids: List of analysis ids
+        """
+        return self._post(endpoint='analyses/status', data={'analysis_ids': analysis_ids})
--- tests/test_franklin.py
+++ tests/test_franklin.py
@@ -8,3 +8,28 @@ def test_get_assay_list(franklin):
 def test_get_analysis_list(franklin):
     assert franklin.get_analysis_list()['0e8389ee-b6ee-41f0-8eca-093e115e7064'] == [217, 183]
 
+
+def test_get_analysis_status(franklin):
+    assert franklin.get_analysis_status([17, 18, 19]) == [
+        {
+            "id": 17,
+            "name": "Demo Sample 1",
+            "status": "ACTIVE",
+            "assay_id": "620434fa-e367-425a-vbnb-n3g7ha10c7a",
+            "created_at": "2018-10-01"
+        },
+        {
+            "id": 18,
+            "name": "Demo Sample 2",
+            "status": "ACTIVE",
+            "assay_id": "620434fa-e367-425a-vbnb-n3g7ha10c7a",
+            "created_at": "2018-10-02"
+        },
+        {
+            "id": 19,
+            "name": "Demo sample 3",
+            "status": "ACTIVE",
+            "assay_id": "620434fa-e367-425a-vbnb-n3g7ha10c7a",
+            "created_at": "2018-10-02"
+        }
+    ]
