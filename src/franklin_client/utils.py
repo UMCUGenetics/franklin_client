af842b4210548658677e1e0357e94f8652295c59
Robert Ernst <r.f.ernst-3@umcutrecht.nl>
2025-10-30 15:11:11 +0100

    Add download_file

--- src/franklin_client/utils.py
+++ src/franklin_client/utils.py
@@ -1,3 +1,10 @@
+import shutil
+
+import requests
+
+from franklin_client.logger import logger
+
+
 def get_file_name_from_aws_url(aws_url):
     """Get file name from AWS URL
 
@@ -27,3 +34,18 @@ def get_file_name_from_headers(response_headers):
             if part.startswith("filename="):
                 return part.split("=")[1].strip('"')
     return None
+
+
+def download_file(file_url, file_name=None):
+    """Download file from URL
+
+    Args:
+        file_url (str): URL to file
+        file_name (str): Name of the file to save
+    """
+    with requests.get(file_url, stream=True) as r:
+        if file_name is None:
+            file_name = get_file_name_from_headers(r.headers)
+        logger.info(f"Downloading file: {file_name}")
+        with open(file_name, "wb") as f:
+            shutil.copyfileobj(r.raw, f)
